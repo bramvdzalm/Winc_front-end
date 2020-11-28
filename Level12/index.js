71070c7b90cca7bf917a185fb9bb83501cd68b20
bramvdzalm <69297451+bramvdzalm@users.noreply.github.com>
2020-11-28 10:17:15 +0100

    Add files via upload

---
+++ Level12/index.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy")
+let naam = "Bram"
+console.log(naam)
+console.log(optellen1)
+let optellen2 = "4" + "4"
+console.log(optellen2)
+let optellen3 = 4 * 4
+ console.log(optellen3)
+let getalA = 2
+let getalB = 5
+let getalC = 7
+console.log(getalA + getalB)
+console.log(getalB * getalC)
+console.log(getalB / getalC)
+console.log(getalA - getalC)
+console.log(getalA % getalC)
+let leeftijd = "32"
+console.log(leeftijd)
+typeof leeftijd
