6fa165205bb9ec8b45e8056230d25ebf5636ba6e
bramvdzalm <69297451+bramvdzalm@users.noreply.github.com>
2020-12-02 13:57:55 +0100

    Add files via upload

---
+++ Level13-ifelse/script.js
@@ -0,0 +1,21 @@
+const age = 18;
+
+if (age > 17) {
+    console.log("jij mag naar binnen!")
+ } else { 
+    console.log("jij mag niet naar binnen, je bent te jong ")
+ }
+
+ const isFemale = true;
+
+ if (isFemale) {
+     console.log("jij bent een madame")
+ }
+
+ const driverStatus = "rob";
+
+ if (driverStatus == "bob") {
+     console.log("jij mag naar huis rijden")
+ } else {
+     console.log("je mag niet rijden, bel een taxi")
+ }
