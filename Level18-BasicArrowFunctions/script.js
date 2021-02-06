67ab0017a45dc29ebd1a2de3f7ed22e3edcfbd0a
bramvdzalm <69297451+bramvdzalm@users.noreply.github.com>
2021-02-06 09:20:53 +0100

    add level18

---
+++ Level18-BasicArrowFunctions/script.js
@@ -0,0 +1,21 @@
+// zet de functie om naar een arrow functie
+
+
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+let fivePlusSeven = () => 5 + 7
+
+let myFunction = (a, b) => a + b
+
+let addFive = a => a + 5
+
+let createObject = () => ({greeting: "hoi"})
+
