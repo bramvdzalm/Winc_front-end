592ea7a599d10531c0a32de131bbdee38fa6ab3f
bramvdzalm <69297451+bramvdzalm@users.noreply.github.com>
2021-02-06 10:28:57 +0100

    Add files via upload

---
+++ Level14/script.js
@@ -0,0 +1,22 @@
+let makePizza = function() {
+    console.log("Making pizza dough")
+    console.log("Making sauce")
+    console.log("Assembling and baking the pizza")
+}
+
+let makeSushi = function() {
+    console.log("Prep the Rice & Nori")
+    console.log("Spread the Rice on the Nori")
+    console.log("Add Fillings to the Bottom Quarter of the Nori")
+    console.log("Roll the Sushi Part Way")
+    console.log("Roll the Sushi the Rest of the Way")
+    console.log("Move to Cutting Board")
+    console.log("Cut the Sushi")
+    console.log("Serve")
+}
+
+makeSushi()
+makeSushi()
+makeSushi()
+makePizza()
+makePizza()
